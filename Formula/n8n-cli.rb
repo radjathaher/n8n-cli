7a381b28ad97112beb4ffae28c50fd13ce5a781b
Radja Thaher <radjapolem@gmail.com>
2026-01-30 20:29:26 +0700

    chore: add homebrew sha

--- Formula/n8n-cli.rb
+++ Formula/n8n-cli.rb
@@ -7,7 +7,7 @@ class N8nCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/radjathaher/n8n-cli/releases/download/v0.1.0/n8n-cli-0.1.0-darwin-aarch64.tar.gz"
-      sha256 "REPLACE_SHA"
+      sha256 "15db0d237722b5bfaed971a8faf9ad3e31236b7cdf94282b6f5ba47e56731ec9"
     else
       odie "n8n-cli is only packaged for macOS arm64"
     end
